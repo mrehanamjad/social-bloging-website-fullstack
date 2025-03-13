7cc2c99ad79d8b5208518d3dfb71874dbaeb92e5
M.Rehan Amjad <rehanamjad814@gmail.com>
2025-03-13 19:29:42 +0500

    ...

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import authService from '../../appwrite/auth'
 import { logout } from '../../store/authSlice'
-import Button from '../Button'
 
 
 function LogoutBtn({className}) {
@@ -10,7 +9,6 @@ function LogoutBtn({className}) {
   const dispach = useDispatch()
 
   const logoutHandler = () => {
-    // these reducers i.e login, logout etc  are promises, most of the methods in appwrite are promises.
     authService.logout().then(() => {
       dispach(logout()).then((ifAnyReturn)=>alert(ifAnyReturn))
     })
