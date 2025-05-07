e8be4c3ee3179ebc9b140391981522166472dcc3
M.Rehan Amjad <rehanamjad814@gmail.com>
2025-05-07 18:30:47 +0500

    ensure resetAll is dispatched after logout

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -4,20 +4,25 @@ import authService from '../../appwrite/auth'
 import { logout } from '../../store/authSlice'
 import { PiSignOutBold } from "react-icons/pi";
 import NavButton from './NavButton'
+import { resetAll } from '../../store/postsSlice';
 
 function LogoutBtn() {
 
   const dispach = useDispatch()
 
   const logoutHandler = () => {
-    authService.logout().then(() => {
-      dispach(logout()).then((ifAnyReturn)=>alert(ifAnyReturn))
-    })
-      .catch((error) => {
-        console, log('Logout failed', error)
+    authService.logout()
+      .then(() => {
+        dispach(logout()).then((ifAnyReturn) => {
+          alert(ifAnyReturn);
+          dispach(resetAll());
+        });
       })
-  }
-
+      .catch((error) => {
+        console.log('Logout failed', error);
+      });
+  };
+  
 
   return <NavButton item={{name:"Logout",slug:"/",icon:PiSignOutBold}} onClick={logoutHandler}/>
 }
